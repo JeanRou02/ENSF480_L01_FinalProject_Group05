dbc9f82a385c77ee0d56c3bae8d0372402bbc66c
Zaid <113048128+zaid-ahmed1@users.noreply.github.com>
2023-11-26 21:36:27 -0700

    Create Crew.java

---
+++ People/Crew.java
@@ -0,0 +1,10 @@
+
+public class Crew extends User{
+	private String position;
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getPosition() {
+		return position;
+	}
+}
